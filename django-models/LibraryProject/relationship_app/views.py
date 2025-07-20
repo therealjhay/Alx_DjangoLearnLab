1d52d1601f1d6f591591cdb7fe755fa05f006a28
Johnson Oyemade <92530531+therealjhay@users.noreply.github.com>
2025-07-21 00:01:56 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import permission_required login_required, user_passes_test, 
 
 from .models import Book, Library, UserProfile
 from .forms import BookForm
