b5754d4ff765e22b60517d3c335d769137415f7c
Johnson Oyemade <92530531+therealjhay@users.noreply.github.com>
2025-07-27 21:51:36 +0100

    Update urls.py

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('books/', views.book_list, name='book_list'),
     path('books/add/', views.book_create, name='book_create'),
+    path('books/search/', views.book_search, name='book_search'), # Add search URL
     path('books/<int:pk>/edit/', views.book_edit, name='book_edit'),
     path('books/<int:pk>/delete/', views.book_delete, name='book_delete'),
     path('login/', views.login_view, name='login'), # Add a login URL
