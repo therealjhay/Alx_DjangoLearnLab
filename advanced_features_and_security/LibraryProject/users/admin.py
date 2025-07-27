55483e581990baa03dc163ea3d8ccb6b6fcecb91
Johnson Oyemade <92530531+therealjhay@users.noreply.github.com>
2025-07-27 19:28:11 +0100

    Update admin.py

--- advanced_features_and_security/LibraryProject/users/admin.py
+++ advanced_features_and_security/LibraryProject/users/admin.py
@@ -4,13 +4,17 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
+    """
+    Custom Admin interface for CustomUser model.
+    Extends the default UserAdmin to display and edit custom fields.
+    """
     model = CustomUser
-    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
+    list_display = UserAdmin.list_display + ('date_of_birth', 'profile_photo',) # Add custom fields to list display
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('date_of_birth', 'profile_photo',)}),
+        ('Custom Fields', {'fields': ('date_of_birth', 'profile_photo',)}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('date_of_birth', 'profile_photo',)}),
+        ('Custom Fields', {'fields': ('date_of_birth', 'profile_photo',)}),
     )
 
 admin.site.register(CustomUser, CustomUserAdmin)
