18a354ce75bbb71e63d2883158118ea3bd0d6771
therealjhay <johnsonoca@gmail.com>
2025-08-22 02:59:26 +0100

    fix

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
 # ✅ List all posts
 class PostListView(ListView):
